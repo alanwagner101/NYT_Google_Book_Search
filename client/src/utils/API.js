5919dfedb2c33e6fb438c2ae1a6183812f50a759
Alan Wagner <alanwagner101@gmail.com>
2019-02-21 22:30:32 -0600

    test

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,19 +3,19 @@ import axios from "axios";
 export default {
   // Gets all books
   getBooks: function() {
-    return axios.get("http://localhost:3001/api/books");
+    return axios.get("https://obscure-harbor-14924.herokuapp.com/api/books");
   },
   // Gets the book with the given id
   getBook: function(id) {
-    return axios.get("http://localhost:3001/api/books/" + id);
+    return axios.get("https://obscure-harbor-14924.herokuapp.com/api/books/" + id);
   },
   // Deletes the book with the given id
   deleteBook: function(id) {
-    return axios.delete("http://localhost:3001/api/books/" + id);
+    return axios.delete("https://obscure-harbor-14924.herokuapp.com/api/books/" + id);
   },
   // Saves a book to the database
   saveBook: function(bookData) {
-    return axios.post("http://localhost:3001/api/books", bookData);
+    return axios.post("https://obscure-harbor-14924.herokuapp.com/api/books", bookData);
   },
   findBooks: function(name) {
     return axios({
